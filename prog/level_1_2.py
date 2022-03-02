c2efce7eaed2d4d9d01c32db7a3ddda928bea739
youngdong82 <nfs82young@gmail.com>
2022-03-02 23:49:19 +0900

    220302 prog level1,2

---
+++ prog/level_1_2.py
@@ -0,0 +1,185 @@
+# -------------------------------------------------------------------------------- 1. 체육복 시간초과 - 복습
+# ------------------------------------------- 내꺼 25분 컷! - 시간초과 
+# def solution(n, lost, reserve):
+#   student = [0 for _ in range(n)]
+#   for i in lost:
+#     student[i-1] -= 1
+#   for i in reserve:
+#     student[i-1] += 1
+
+#   print(student)
+#   for i in range(n):
+#     if i > 0 and student[i] == 1 and student[i-1] == -1:
+#       student[i-1] = 0
+#       student[i] = 0
+#     elif i < n-1 and student[i] == 1 and student[i+1] == -1: 
+#       student[i+1] = 0
+#       student[i] = 0
+#   print(student)
+
+#   answer = 0
+#   for i in student:
+#     if i >= 0:
+#       answer += 1
+#   return answer
+
+# print(solution(5, [2,4], [1,3,5]))
+# print(solution(5, [2,4], [3]))
+# print(solution(5, [2], [3]))
+# print(solution(3, [3], [1]))
+# print(solution(2, [0], [1]))
+# print(solution(2, [1], [1]))
+
+# -------------------------------------------------------------------------------- 2. 없는 숫자 더하기
+# ------------------------------------------- 내꺼 3분 컷!
+# def solution(numbers):
+#   answer = []
+#   for i in range(10):
+#     if i not in numbers:
+#       answer.append(i)
+#   return sum(answer)
+
+# print(solution([1,2,3,4,6,7,8,0]))
+# print(solution([5,8,4,0,6,7,9]))
+
+# -------------------------------------------------------------------------------- 3. 같은 숫자는 싫어 - 복습!
+# ------------------------------------------- 내꺼 2번째 시도에서 5분 컷! 근데 이상하게 헤맸었다!
+def solution(a):
+  n = len(a)
+  answer = [a[0]]
+  for i in range(1,n):
+    if a[i] != answer[-1]:
+      answer.append(a[i])
+
+  return answer
+
+
+print(solution([1,1,3,3,0,1,1]))
+print(solution([4,4,4,3,3]))
+print(solution([4,4,4,4,4]))
+print(solution([1,2]))
+
+# -------------------------------------------------------------------------------- 4. 평균 구하기 
+# ------------------------------------------- 내꺼 1분 컷!
+# def solution(arr):
+#   a = sum(arr)/len(arr)
+#   return round(a, 2)
+
+# print(solution([1, 4, 5]))
+# print(solution([1,2,3,4]))
+# print(solution([5,5]))
+
+# -------------------------------------------------------------------------------- 5. 자릿수 더하기
+# ------------------------------------------- 내꺼 3분 컷!
+# def solution(n):
+#   answer = sum(list(map(int,str(n))))
+#   return answer
+
+# print(solution(123))
+# print(solution(987))
+
+# -------------------------------------------------------------------------------- 6. 문자열 다루기 기본 - 시간초과!! 복습!
+# ------------------------------------------- 내꺼 아 어이없게...ㅋㅋㅋㅋㅅㅂ 16분 컷
+# def solution(s):
+#   if len(s) == 4 or len(s) == 6:
+#     for i in s:
+#       if i.isdigit() == False:
+#         return False
+#     return True
+#   return False
+
+# ------------------------------------------- 커뮤니티ㅋㅋㅋㅋ
+# def solution(s):
+#   return len(s) in [4,6] and s.isdigit()
+
+# -------------------------------------------------------------------------------- 7. 문자열을 정수로 바꾸기
+# ------------------------------------------- 내꺼 7분 컷!
+# def solution(s):
+#   answer = 0
+#   if s[0] == '+':
+#     answer = int(s[1:])
+#   elif s[0] == '-' :
+#     answer = -int(s[1:])
+#   else:
+#     answer = int(s)
+#   return answer
+
+# ------------------------------------------- 커뮤니티 ㅋㅋㅋㅋㅋㅋㅋㅅㅂ 개쩌네
+# def solution(s):
+#   return int(s)
+
+# print(solution('+1234'))
+# print(solution('1234'))
+# print(solution('-1234'))
+
+# -------------------------------------------------------------------------------- 8. 소수 만들기
+# ------------------------------------------- 내꺼 11분 컷!
+# from math import sqrt
+# from itertools import combinations
+
+
+# def prime_num(n):
+#   for i in range(2,int(sqrt(n)) + 1):
+#     if n % i == 0:
+#       return False
+#   return True
+
+# def solution(nums):
+#   count = 0
+#   num_lists = list(combinations(nums,3))
+#   for num in num_lists:
+#     if prime_num(sum(num)):
+#       count += 1
+#   return count
+
+
+# print(solution([1,2,3,4]))
+# print(solution([1,2,7,6,4]))
+
+# -------------------------------------------------------------------------------- 9. 약수의 개수와 덧셈
+# ------------------------------------------- 내꺼 11분 컷!
+# from math import sqrt
+
+# def solution(left, right):
+#   answer = 0
+#   for i in range(left, right + 1):
+#     if int(sqrt(i)) != sqrt(i):
+#       answer += i
+#     else:
+#       answer -= i
+#   return answer
+
+# print(solution(13,17))
+# print(solution(24,27))
+
+# -------------------------------------------------------------------------------- 10. 부족한 금액 계산하기
+# ------------------------------------------- 내꺼 12분 컷!
+# def solution(price, money, count):
+#   pay = 0
+#   for i in range(1, count+1):
+#     pay += (price * i)
+#   if money >= pay:
+#     return 0
+#   else:
+#     return pay - money
+
+# print(solution(3,20,4))
+
+# -------------------------------------------------------------------------------- 11. 나머지가 1이 되는 수 찾기
+# ------------------------------------------- 내꺼 5분 컷!
+# def solution(n):
+#   for i in range(1,n):
+#     if n%i == 1:
+#       return i
+
+# print(solution(10))
+# print(solution(12))
+
+# -------------------------------------------------------------------------------- 12. 자연수 뒤집어 배열로 만들기
+# ------------------------------------------- 내꺼 5분 컷!
+# def solution(n):
+#   a = list(map(int,str(n)))
+#   a.reverse()
+#   return a
+
+# print(solution(12345))
--- prog/level_2_1.py
+++ prog/level_2_1.py
@@ -1,3 +1,11 @@
+# -------------------------------------------------------------------------------- divmod
+# 몫과 나머지를 튜플로 묶어서 return
+# a = 6
+# b = 3
+# c,d = divmod(a, b)
+# -------------------------------------------------------------------------------- array[::-1]
+
+
 # -------------------------------------------------------------------------------- 1. 오픈채팅방
 # ------------------------------------------- 내꺼
 # dic쓸거 list 써서 개고생했네 진짜....
@@ -27,39 +35,195 @@
 #   "Change uid4567 Ryan"
 #   ]))
 
-# -------------------------------------------------------------------------------- 2. 짝지어 제거하기
+# -------------------------------------------------------------------------------- 2. 짝지어 제거하기 아직 못풀었음
 # ------------------------------------------- 내꺼
+# def solution(s):
+#   answer = []
+#   s = list(s)
 
-def solution(s):
-  answer = []
-  s = list(s)
+#   def reduce(s):
+#     if len(s) == 0:
+#       answer.append(1)
+#     for i in range(len(s)-1):
+#       if s[i] == s[i+1]:
+#         s.remove(s[i])
+#         s.remove(s[i])
+#         reduce(s)
+#         break
 
-  def reduce(s):
-    if len(s) == 0:
-      answer.append(1)
-    for i in range(len(s)-1):
-      if s[i] == s[i+1]:
-        s.remove(s[i])
-        s.remove(s[i])
-        reduce(s)
-        break
-
-  reduce(s)
-  if len(answer) == 0:
-    return 0
-  else:
-    return 1
+#   reduce(s)
+#   if len(answer) == 0:
+#     return 0
+#   else:
+#     return 1
       
       
 
-print(solution('baabaa'))
-print(solution('cdcd'))
+# print(solution('baabaa'))
+# print(solution('cdcd'))
+
+# -------------------------------------------------------------------------------- 3. 최솟값 만들기
+# 배열 A, B에서 각각 한 개의 숫자를 뽑아 두 수를 곱합니다.
+# 이러한 과정을 배열의 길이만큼 반복하며,
+# 두 수를 곱한 값을 누적하여 더합니다
+# 중복선택 불가
+# ------------------------------------------- 내꺼 시간초과
+# def solution(A,B):
+#   answer = 0
+#   for i in range(len(A)):
+#     answer += min(A)*max(B)
+#     A.remove(min(A))
+#     B.remove(max(B))
+#   return answer
+
+# ------------------------------------------- 내꺼 2분컷...근데 뻘짓하느라 20분을 날려먹었다.
+# def solution(A,B):
+#   n = len(A)
+#   answer = 0
+#   A.sort()
+#   B.sort(reverse=True)
+
+#   for i in range(n):
+#     answer += A[i] * B[i]
+#   return answer
+
+# print(solution([1, 4, 2], [5, 4, 4]))
+# print(solution([1,2], [3,4]))
+
+# -------------------------------------------------------------------------------- 4. 땅따먹기  - 실패! - 복습!!!!
+# ------------------------------------------- 내꺼 시간초과
+# def max_index(array, ban):
+#   max_value = 0
+#   index = 0
+#   for i in range(4):
+#     if ban != None:
+#       if array[i] > max_value and i != ban:
+#         max_value = array[i]
+#         index = i
+#     else:
+#       if array[i] > max_value:
+#         max_value = array[i]
+#         index = i
+#   return max_value, index
 
 
-# a = [1,2,2,4,5]
+# def solution(array):
+#   n = len(array)
+#   maxx, index = max_index(array[0], None)
+#   answer = maxx
+
+#   for i in range(1,n):
+#     maxx, index = max_index(array[i], index)
+#     answer += maxx
+
+#   return answer
+# ------------------------------------------- 커뮤니티 와 이건 미쳤다 
+# def solution(land):
+#   for i in range(1,len(land)):
+#       for j in range(4):
+#           land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+#   return max(land[-1])
 
 
-# for i in range(len(a)-1):
-#   if a[i] == a[i+1]:
-#     a.remove(a[i])
-#     a.remove(a[i])
+# print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
+# -------------------------------------------------------------------------------- 5. 124나라의 숫자 실패! - 복습!!
+# ------------------------------------------- 내꺼
+# def solution(n):
+#   answer = []
+#   a = 1
+#   while True:
+#     if n < (3**a):
+#       break
+#     else:
+#       a += 1
+#   for i in range(a-1,-1,-1):
+#     if i == 0:
+#       answer.append(n)
+#       break
+#     answer.append(n // (3**i))
+#     n -= (3**i)
+
+#   return answer
+
+# ------------------------------------------- 커뮤니티
+# def solution(n):
+#     answer = ''
+#     while n:
+#         if n % 3:
+#             answer += str(n % 3)
+#             n //= 3
+#         else:
+#             answer += "4"
+#             n = n//3 - 1
+#     return answer[::-1]
+
+# ------------------------------------------- 커뮤니티 2 이게 더 깔끔 이걸 어떻게 알아내냐...
+# def change124(n):
+#     num = ['1','2','4']
+#     answer = ""
+#     while n > 0:
+#         n -= 1
+#         answer = num[n % 3] + answer
+#         n //= 3
+
+#     return answer
+# print(solution(11))
+# print(solution(12))
+# print(solution(13))
+# print(solution(14))
+# print(solution(15))
+# -------------------------------------------------------------------------------- 6. 게임 맵 최단거리
+# ------------------------------------------- 내꺼 시간초과 32분 컷!
+# from collections import deque
+
+# def solution(maps):
+#   n = len(maps)
+#   m = len(maps[0])
+#   dx = [0,1,0,-1]
+#   dy = [-1,0,1,0]
+
+#   def bfs(graph, x,y):
+#     q = deque([(x,y)])
+#     graph[y][x] = 2
+#     while q:
+#       now = q.popleft()
+#       now_x, now_y = now
+#       for i in range(4):
+#         nx = now_x + dx[i]
+#         ny = now_y + dy[i]
+#         if 0 <= nx < m and 0 <= ny < n:
+#           if graph[ny][nx] == 1:
+#             graph[ny][nx] = graph[now_y][now_x] + 1
+#             q.append((nx,ny))
+
+#   bfs(maps, 0,0)
+#   if maps[n-1][m-1] in [0,1]:
+#     return -1
+#   else:
+#     return maps[n-1][m-1] -1
+
+# print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1]]))
+# print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
+
+# -------------------------------------------------------------------------------- 7. 숫자의 표현
+# ------------------------------------------- 내꺼 20분 컷!
+def solution(n):
+  summ = 0
+  count = 0
+  index1 = 1
+  index2 = 1
+  while index1 <= n//2 + 1 or index2 <= n//2 + 1:
+    if summ < n:
+      summ += index2
+      index2 += 1
+    elif summ > n:
+      summ -= index1
+      index1 += 1
+    else:
+      count += 1
+      summ += index2
+      index2 += 1
+  return count + 1
+
+
+print(solution(15))
